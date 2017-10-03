bc07230c741236d21bc7ec463cf7b55390945fdb
Ambareesh <ambareesh_b@yahoo.com>
2017-10-04 05:55:08 +0800

    The first commit. Committing all the files created by spring boot

---
+++ src/main/java/com/ab/java/spring/studies/one/bookstore/BookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.ab.java.spring.studies.one.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+}
